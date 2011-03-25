e3111cbd95710fcd7f3799bb178a8186e49e7dc3
Sylvester Keil <sylvester@keil.or.at>
2011-03-25 20:38:43 +0100

    Added Node spec.

---
+++ spec/csl/node_spec.rb
@@ -0,0 +1,15 @@
+module CSL
+  describe Node do
+    
+    describe 'CSL.Node utility function' do
+      # it 'returns a new Node from an empty Hash' do
+      #   CSL.Node({}).should_not be_nil
+      # end
+    end
+    
+    describe '#new' do
+      it { should_not be_nil }
+    end
+    
+  end
+end
