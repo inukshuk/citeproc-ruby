e260e0e27e86a31081396d0a2c2e63fcf59de245
Charles Pence <charles@charlespence.net>
2012-01-31 20:33:39 -0500

    Support more Unicode gems; Ruby 1.8 compatibility.
    The Unicode upper and lowercase conversions can be provided by the
    'unicode' gem or the native Java support in JRuby.  Support both
    of those.  Also, the method names #id and #type are used throughout
    the citeproc-ruby code.  Those are methods defined on Object in
    Ruby 1.8.  Undefine them (if you need them, use #object_id and

--- lib/support/compatibility.rb
+++ lib/support/compatibility.rb
@@ -1,42 +1,83 @@
 
-begin
-  require 'unicode_utils'
+# Remove the 'id' and 'type' methods in Ruby 1.8, as they're used all over
+# the source
+if RUBY_VERSION < "1.9.0"
+  class Object
+    undef_method :id
+    undef_method :type
+  end
+end
 
+# Robust solutions for Unicode
+if RUBY_PLATFORM == 'java'
+  require 'java'
+  
   module Support
     def self.upcase(string)
-      UnicodeUtils.upcase(string)
+      java.lang.String.new(string).to_upper_case(java.util.Locale::ENGLISH).to_s
     end
     
     def self.downcase(string)
-      UnicodeUtils.downcase(string)
+      java.lang.String.new(string).to_lower_case(java.util.Locale::ENGLISH).to_s
     end
   end
   
-rescue LoadError
+else
+
   begin
-    require 'active_support/multibyte/chars'
+    require 'unicode'
     
     module Support
       def self.upcase(string)
-        ActiveSupport::Multibyte::Chars.new(string).upcase.to_s
+        Unicode.upcase(string)
       end
       
       def self.downcase(string)
-        ActiveSupport::Multibyte::Chars.new(string).downcase.to_s
+        Unicode.downcase(string)
       end
     end
     
   rescue LoadError
+    begin
+      require 'unicode_utils'
+
+      module Support
+        def self.upcase(string)
+          UnicodeUtils.upcase(string)
+        end
     
-    module Support
-      def self.upcase(string)
-        string.upcase
+        def self.downcase(string)
+          UnicodeUtils.downcase(string)
+        end
       end
+  
+    rescue LoadError
+      begin
+        require 'active_support/multibyte/chars'
+    
+        module Support
+          def self.upcase(string)
+            ActiveSupport::Multibyte::Chars.new(string).upcase.to_s
+          end
       
-      def self.downcase(string)
-        string.downcase
+          def self.downcase(string)
+            ActiveSupport::Multibyte::Chars.new(string).downcase.to_s
+          end
+        end
+    
+      rescue LoadError
+    
+        module Support
+          def self.upcase(string)
+            string.upcase
+          end
+      
+          def self.downcase(string)
+            string.downcase
+          end
+        end
+      
       end
     end
-    
   end
 end
