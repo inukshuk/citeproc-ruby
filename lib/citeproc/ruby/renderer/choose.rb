dc4972ed71a516d615a9776c25621e1d09f29478
Sylvester Keil <sylvester@keil.or.at>
2013-01-20 22:23:34 +0100

    add choose and block renderer

--- lib/citeproc/ruby/renderer/choose.rb
+++ lib/citeproc/ruby/renderer/choose.rb
@@ -9,9 +9,32 @@ module CiteProc
       # @param node [CSL::Style::Choose]
       # @return [String]
       def render_choose(item, node)
+        return '' unless node.has_children?
         
+        node.each_child do |child|
+          return render_block(item, child) if evaluate_block(item, child)
+        end
+
+        '' # no block was rendered
       end
       
+      # @param item [CiteProc::CitationItem]
+      # @param node [CSL::Style::Choose::Block]
+      # @return [String]
+      def render_block(item, node)
+        return '' unless node.has_children?
+        
+        node.each_child.map { |child|
+          render item, child
+        }.join('')
+      end
+
+      def evaluate_block(item, block)
+        # case
+        # when
+        # else
+        # end
+      end
     end
     
   end
