bf2ed5bd8a603ff327505d49cf56fa0cdb8bb592
Sylvester Keil <sylvester@keil.or.at>
2013-01-22 19:17:55 +0100

    wip names rendering

--- lib/citeproc/ruby/renderer/names.rb
+++ lib/citeproc/ruby/renderer/names.rb
@@ -1,18 +1,44 @@
 module CiteProc
   module Ruby
-    
+
     class Renderer
-      
-      private
 
       # @param item [CiteProc::CitationItem]
       # @param node [CSL::Style::Names]
       # @return [String]
       def render_names(item, node)
-        
+        return '' unless node.has_variable?
+
+        names = node.variable.split(/\s+/).map do |role|
+          [role.to_sym, item.data[role]]
+        end
+
+        resolve_editor_translator_exception names
+
+
       end
-      
+
+      # @param name [CiteProc::Name]
+      # @param node [CSL::Style::Name]
+      # @return [String]
+      def render_name(name, node)
+      end
+
+      private
+
+      def resolve_editor_translator_exception(names)
+        editor, translator = names[:editor], names[:translator]
+
+        if translator && translator == editor
+          names.delete :editor
+          names.delete :translator
+
+          names[:editortranslator] = editor
+        end
+
+        names
+      end
     end
-    
+
   end
 end
