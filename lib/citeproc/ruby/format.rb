2a167921fd20a711877a0021bab5017c6ef61f49
Sylvester Keil <sylvester@keil.or.at>
2014-01-10 12:37:40 +0100

    implement default format

--- lib/citeproc/ruby/format.rb
+++ lib/citeproc/ruby/format.rb
@@ -29,13 +29,105 @@ module CiteProc
         self.class.name
       end
 
-      def apply(string, node)
-        string, options = string.dup, node.formatting_options
+      def keys
+        @keys ||= (CSL::Schema.attr(:formatting) - [:prefix, :suffix, :display])
+      end
 
-        string.prepend(options[:prefix]) if options.key?(:prefix)
-        string.concat(options[:suffix]) if options.key?(:suffix)
+      def apply(input, node)
+        return '' if input.nil?
+        return input if input.empty? || node.nil?
 
-        string
+        @input, @output, @node = input, input.dup, node
+
+        setup!
+
+        unless options.empty?
+
+          # NB: Layout nodes apply formatting to
+          # affixes; all other nodes do not!
+          if node.is_a? CSL::Style::Layout
+            apply_prefix if options.key?(:prefix)
+            apply_suffix if options.key?(:suffix)
+          end
+
+          keys.each do |format|
+            if options.key?(format)
+              method = "apply_#{format}".tr('-', '_')
+              send method if respond_to?(method)
+            end
+          end
+
+          output.gsub! /\./, '' if node.strip_periods?
+
+          # TODO quotes needs locale
+
+          finalize_content!
+
+          unless node.is_a? CSL::Style::Layout
+            apply_prefix if options.key?(:prefix)
+            apply_suffix if options.key?(:suffix)
+          end
+
+          apply_display if options.key?(:display)
+        end
+
+        finalize!
+
+        output
+      ensure
+        cleanup!
+      end
+
+      def apply_text_case
+        case options[:'text-case']
+        when 'lowercase'
+          output.replace CiteProc.downcase output
+
+        when 'uppercase'
+          output.replace CiteProc.upcase output
+
+        when 'capitalize-first'
+          output.sub! /^(\p{Ll})/, CiteProc.upcase($1)
+
+        when 'capitalize-all'
+          output.gsub! /\b(\p{Ll})/, CiteProc.upcase($1)
+
+        when 'sentence'
+          output.gsub! /\b\p{Lu}(\p{Lu}+)\b/, CiteProc.downcase($1)
+          output.gsub! /\b(\p{Ll})\p{Ll}*\b/, CiteProc.upcase($1)
+
+        when 'title'
+          # TODO needs locale stop words
+        end
+      end
+
+      def apply_prefix
+        output.prepend(options[:prefix])
+      end
+
+      def apply_suffix
+        output.concat(options[:suffix])
+      end
+
+      protected
+
+      attr_reader :input, :output, :node
+
+      def options
+        @options ||= @node.formatting_options
+      end
+
+      def finalize!
+      end
+
+      def finalize_content!
+      end
+
+      def setup!
+      end
+
+      def cleanup!
+        @input, @output, @node, @options = nil
       end
     end
 
--- lib/citeproc/ruby/renderer/layout.rb
+++ lib/citeproc/ruby/renderer/layout.rb
@@ -1,20 +1,18 @@
 module CiteProc
   module Ruby
-    
+
     class Renderer
-      
-      private
 
       # @param item [CiteProc::CitationItem]
       # @param node [CSL::Style::Layout]
       # @return [String]
       def render_layout(item, node)
-        node.each_child.map { |child|
+        format node.each_child.map { |child|
           render item, child
-        }.reject(&:empty?).join(node.delimiter)
+        }.reject(&:empty?).join(node.delimiter), node
       end
-      
+
     end
-    
+
   end
 end
